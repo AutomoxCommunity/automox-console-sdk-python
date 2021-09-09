80cd166c9ba29bb307f0b9c8578b54188048077f
Zachary Youtz <zachary.youtz@automox.com>
2021-09-09 10:02:48 -0600

    Publish packages with license, classifiers, and long description; set setup.py VERSION during generation; ignore setup.py during generation

--- setup.py
+++ setup.py
@@ -14,6 +14,11 @@ from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "automox-console-sdk"
 VERSION = "0.1.0"
+DESCRIPTION = "Automox Console SDK for Python"
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 # To install the library, run the following
 #
 # python setup.py install
@@ -26,14 +31,22 @@ REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 setup(
     name=NAME,
     version=VERSION,
-    description="Automox Console API",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email="support@automox.com",
     url="https://github.com/AutomoxCommunity/automox-console-sdk-python",
     keywords=["Swagger", "Automox Console API"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    API for use with the Automox Console  # noqa: E501
-    """
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.9'
+    ]
 )
