4b10d5959ac256df08f635bb5ca948145ec0a9ce
Zac Youtz <86067902+ax-zyoutz@users.noreply.github.com>
2021-09-09 12:15:01 -0600

    Update README with pip install instruction, bump version to 0.1.1 (#15)
    * Update README with pip install instruction, bump version to 0.1.1
    
    * Automated changes from swagger codegen generation
    
    * Update example version in readme
    
    Co-authored-by: ax-zyoutz <ax-zyoutz@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "automox-console-sdk"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 DESCRIPTION = "Automox Console SDK for Python"
 
 with open('README.md', 'r') as fh:
